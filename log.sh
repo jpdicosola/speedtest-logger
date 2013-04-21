1539a77578d061d3b17ca9c5bdfd8bac58dec306
johnd <johnd@johnd-VirtualBox.(none)>
2013-04-21 14:54:37 -0500

    Use host instead of curl

--- log.sh
+++ log.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
 fileName="speed_log.txt"
-IPData=$(curl -s ifconfig.me/all)
-IPAddress=$(echo $IPData | awk '{print $2}')
-HostName=$(echo $IPData | awk '{print $4}')
+IPAddress=$(curl -s ifconfig.me)
+HostName=$(host $IPAddress | awk '{print $5}')
 
 echo $(date) >> $fileName
 echo $IPAddress >> $fileName
 echo $HostName >> $fileName
 
-../speedtset-cli/speedtest-cli --simple >> $fileName 
+../speedtest-cli/speedtest-cli --simple >> $fileName 
 echo >> $fileName
